341080704ae3f3d010a063a4a970d731c932e431
Juanes-GH <jjgohe3@gmail.com>
2021-08-05 12:50:18 +0200

    The same exercise but with jsx

--- main.js
+++
@@ -1,8 +0,0 @@
-const $app = document.getElementById('app');
-const h = React.createElement;
-
-const Avatar = params =>{
-    const src=`https://randomuser.me/api/portraits/men/${params.id}.jpg`;
-    return h('img', {src})
-};
-ReactDOM.render(h(Avatar,{id : 5}), $app)
